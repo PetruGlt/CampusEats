b5e440b6bc8301bf379925a56813d7d024251f7d
mrbogdanovich <bogdan.bzn03@protonmail.com>
2025-11-24 13:47:31 +0200

    Yes

--- CampusEats/obj/Debug/net9.0/CampusEats.AssemblyInfo.cs
+++ CampusEats/obj/Debug/net9.0/CampusEats.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusEats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c396850fcc784c20d78417a758e4ce73053b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5013514ca4507f588e35ce900d5f65278ea05ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusEats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusEats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
