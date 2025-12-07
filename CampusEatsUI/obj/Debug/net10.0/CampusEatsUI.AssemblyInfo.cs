b5318b991b086985cfd477b53714efa79441d263
mrbogdanovich <bogdan.bzn03@protonmail.com>
2025-12-07 17:30:06 +0200

    commit

--- CampusEatsUI/obj/Debug/net10.0/CampusEatsUI.AssemblyInfo.cs
+++ CampusEatsUI/obj/Debug/net10.0/CampusEatsUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusEatsUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466c15495e144ab292b304147c17b6cbd3d8b2fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf37da9168fdfbd3fe222f4333c258bd5a1fe79")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusEatsUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusEatsUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
