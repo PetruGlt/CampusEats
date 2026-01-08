0575d190462a97e7d9eb10399e78fb4853297d97
mrbogdanovich <bogdan.bzn03@protonmail.com>
2026-01-08 21:18:44 +0200

    Trying to dev-deploy

--- CampusEats.Tests/obj/Debug/net9.0/CampusEats.Tests.AssemblyInfo.cs
+++ CampusEats.Tests/obj/Debug/net9.0/CampusEats.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusEats.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbcad0e08d4bef86fb3d3ce32ca7838fa8ba9be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9102612e1b2969197ef5db369289c5916a93c2d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusEats.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusEats.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
